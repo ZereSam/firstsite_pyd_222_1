774d7993e6c4553a129edaf367354693c7d6e712
Зере Самбетова <@itstep.loc>
2023-04-26 17:26:35 +0600

    lesson_04 files

--- firstsite/bboard/templates/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from bboard.models import Bb
-
-
-class BbForm(ModeForm):
-    class Meta:
-        model = Bbfields = ('title', 'content', 'price', 'rubric')
--- lesson_04/second/urls.py
+++ lesson_04/second/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for firstsite project.
+URL configuration for second project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -14,13 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.contrib import admin
 from django.urls import path
 
-from bboard.views import index, by_rubric, BbCreateView
-
 urlpatterns = [
-    path('', index, name='index'),
-    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
-    path('add/', BbCreateView.as_view(), name='add'),
+    path('admin/', admin.site.urls),
 ]
