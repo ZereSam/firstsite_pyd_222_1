0bb6b531f0dc90d1538b227e4b79219cb88293cf
ZereS <zere.sambetova@gmail.com>
2023-06-05 17:48:10 +0600

    lesson_13

--- bboard/urls.py
+++ bboard/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 
 from bboard.views import index, by_rubric, BbCreateView
 
+vals = {'name': 'by_index',
+        'beaver': 'beaver - это бобер!'
+}
+
 urlpatterns = [
     path('', index, name='index'),
-    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
+    path('<int:rubric_id>/', by_rubric, vals, name='by_rubric'),
     path('add/', BbCreateView.as_view(), name='add'),
 ]
--- testapp/views.py
+++ testapp/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView, ListView, DeleteView, UpdateView
+
+from testapp.forms import SMSCreateForm
+from testapp.models import SMS
+
 
 # Create your views here.
+class AddSms(CreateView):
+    template_name = 'testapp/create.html'
+    form_class = SMSCreateForm
+    success_url = reverse_lazy('index')
+
+
+class ReadSms(DetailView):
+    model = SMS
+    template_name = 'testapp/read.html'
+
+
+# class DeleteSms(CreateView):
+#     template_name = 'testapp/create.html'
+#     form_class = SMSCreateForm
+#     success_url = reverse_lazy('index')
